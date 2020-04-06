de3dffc5c0f810febd475467b3151b91d2f114a5
unknown <hamish.tana24@gmail.com>
2020-04-07 11:00:16 +1200

    added div and styling

---
+++ client/components/Pixel.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Pixel extends React.Component {
+    render () {
+        return (
+            <div style={{
+                height: '50px',
+                width: '50px',
+                backgroundColor: 'blue'
+            }}>
+            </div>
+        )
+    }
+}
