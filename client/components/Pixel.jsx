7aa46b05fef013d9dc78b31e6a550574ac7ac8ac
Matthew Kalopulu <matthew_kalopulu@hotmail.com>
2020-04-07 10:56:37 +1200

    add Pixel.jsx file and contents

---
+++ client/components/Pixel.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Pixel = () => {
+    return (
+        <div style = {{
+            height: '10px',
+            width: '10px',
+            backgroundColor: 'blue'
+        }}>
+
+        </div>
+    )
+}
+
+export default Pixel
