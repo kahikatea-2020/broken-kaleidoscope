8bfdb9d95955ff8128ecb3457faa86503d6dee3d
Tom Clayton <tgclayton1@gmail.com>
2020-04-07 11:31:09 +1200

    make pixels pretty

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 
 class Pixel extends React.Component {
-  constructor () {
+
+  constructor() {
     super()
+    const randomHexColor = () => `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+    let color = randomHexColor()
+    console.log(color)
     this.state = {
       style: {
-        backgroundColor: 'chartreuse',
+        backgroundColor: color,
         height: '50px',
         width: '50px'
       }
     }
   }
-  render () {
+  render() {
     return (
       <div style={this.state.style}></div>
     )
