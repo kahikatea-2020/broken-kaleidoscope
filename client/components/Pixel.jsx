770bb1d2ce86bb8d5a9e2b173df4390526b0a45a
Lache Melvin <lache.melvin@gmail.com>
2020-04-07 11:18:26 +1200

    make Pixel a class based component with state

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 
-const Pixel = () => {
-  return (
-    <div style = {{
-      backgroundColor: 'chartreuse',
-      height: '50px',
-      width: '50px'
-    }}
-    ></div>
-  )
+class Pixel extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      style: {
+        backgroundColor: 'chartreuse',
+        height: '50px',
+        width: '50px'
+      }
+    }
+  }
+  render () {
+    return (
+      <div style={this.state.style}></div>
+    )
+  }
 }
 
 export default Pixel
