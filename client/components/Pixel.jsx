f6f453ea10b33a50c864ac54427adfe3a53b3267
Ellora Virtue <ellora.virtue@hotmail.com>
2020-04-07 10:51:09 +1200

    Add style to div using JSX attribute

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Pixel = () => {
   return (
-    <div></div>
+    <div style={{
+      height: '5em',
+      width: '5em',
+      backgroundColor: 'bisque'
+    }}></div>
   )
 }
 
