e258f2749447e9905b44529e71c564d408823370
Tom Clayton <tgclayton1@gmail.com>
2020-04-07 11:44:19 +1200

    make colours change on mouseover

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -2,22 +2,29 @@ import React from 'react'
 
 class Pixel extends React.Component {
 
-  constructor() {
-    super()
-    const randomHexColor = () => `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
-    let color = randomHexColor()
-    console.log(color)
-    this.state = {
+  randomHexColor = () => `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+
+  handleClick = () => {
+    this.setState({
       style: {
-        backgroundColor: color,
+        backgroundColor: this.randomHexColor(),
         height: '50px',
         width: '50px'
       }
+    })
+  }
+
+  state = {
+    style: {
+      backgroundColor: this.randomHexColor(),
+      height: '50px',
+      width: '50px'
     }
   }
+
   render() {
     return (
-      <div style={this.state.style}></div>
+      <div onMouseOver={this.handleClick} style={this.state.style}></div>
     )
   }
 }
