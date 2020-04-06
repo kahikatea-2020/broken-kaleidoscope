21f8ec7275de6c7724b179f7bfb18a77b62fdaf6
Ellora Virtue <ellora.virtue@hotmail.com>
2020-04-07 11:49:12 +1200

    Put styles into state

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 
-const Pixel = () => {
-  return (
-    <div style={{
-      height: '5em',
-      width: '5em',
-      backgroundColor: 'bisque'
-    }}></div>
-  )
+class Pixel extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      style: {
+        height: '5em',
+        width: '5em',
+        backgroundColor: 'bisque'
+      }
+    }
+  }
+  render () {
+    return (
+      <div style={this.state.style}></div>
+    )
+  }
 }
 
 export default Pixel
