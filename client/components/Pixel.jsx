ddd1ea4fc4cf4892c8d2aca168fc9a18fb58735f
Matthew Kalopulu <matthew_kalopulu@hotmail.com>
2020-04-07 16:22:00 +1200

    add mouseover and rightclick functions to Pixel.jsx

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -18,14 +18,45 @@ class Pixel extends React.Component {
     clickHandler = evt => {
         this.setState({
             style: {
+                height: '100px',
+                width: '100px',
                 backgroundColor: randomHexColor()
             }
         })
     }
-    
+
+    mouseOver = evt => {
+        this.setState({
+            style: {
+                height: '100px',
+                width: '100px',
+                backgroundColor: 'green'
+            }
+        })
+    }
+
+    rightClick = evt => {
+        evt.preventDefault()
+        this.setState({
+            style: {
+                height: '100px',
+                width: '100px',
+                backgroundColor: 'black'
+            }
+        })
+    }
+
     render() {
         return (
-            <div onClick={this.clickHandler} style = {this.state.style}></div>
+            <div 
+            onMouseEnter={this.mouseOver} 
+            onClick={this.clickHandler} 
+            style={this.state.style}
+            onContextMenu = {this.rightClick}
+            
+            >
+
+            </div>
         )
     }
 }
