865909f525ae6ab5e77c6843d3cabe16e42fd3cd
Ellora Virtue <ellora.virtue@hotmail.com>
2020-04-07 16:51:13 +1200

    Add onDoubleClick changes colour to white

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -36,6 +36,27 @@ class Pixel extends React.Component {
     })
   }
 
+  changeToBlack = evt => {
+    evt.preventDefault()
+    this.setState({
+      style: {
+        height: '5em',
+        width: '5em',
+        backgroundColor: 'black'
+      }
+    })
+  }
+
+  changeToWhite = evt => {
+    this.setState({
+      style: {
+        height: '5em',
+        width: '5em',
+        backgroundColor: 'white'
+      }
+    })
+  }
+
   render () {
     return (
       <>
@@ -43,6 +64,8 @@ class Pixel extends React.Component {
           style={this.state.style}
           onClick={this.changeToRandom}
           onMouseEnter={this.changeToGreen}
+          onContextMenu={this.changeToBlack}
+          onDoubleClick={this.changeToWhite}
         >
         </div>
       </>
