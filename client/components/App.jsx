3bfa88c3a885d04e1df41620bb87189a10bdcf03
malyz <mattuy001@gmail.com>
2020-04-07 18:57:24 +1200

    added variables

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,24 +2,31 @@ import React from "react";
 import Pixel from "./Pixel";
 import { v4 as uuidv4 } from "uuid";
 
+const boxNum = 1092;
+const boxSize = "45px";
+
 const generateBoxes = (num) =>
   Array.from({ length: num }, (v, i) => (
-    <Pixel key={uuidv4()} height="40px" width="40px" />
+    <Pixel key={uuidv4()} height={boxSize} width={boxSize} />
   ));
 
 class App extends React.Component {
   state = {
-    boxes: generateBoxes(1082),
+    boxes: generateBoxes(boxNum),
   };
 
   componentDidMount() {
     setInterval(() => {
-      this.setState({ boxes: generateBoxes(1082) });
+      this.setState({ boxes: generateBoxes(boxNum) });
     }, 2000);
   }
 
   render() {
-    return <div>{this.state.boxes}</div>;
+    return (
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        {this.state.boxes}
+      </div>
+    );
   }
 }
 
