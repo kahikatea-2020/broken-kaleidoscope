152610dd3c406e65d5db22ac2726d8935849eef2
Karel Fourie <karelmakazi@gmail.com>
2020-04-07 12:37:57 +1200

    Add extra interactivity options

---
+++ client/components/Pixel.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+
+const randomHexColor = () =>
+`#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+
+class Pixel extends React.Component{
+
+  state = {
+    style:{
+      backgroundColor: randomHexColor(),
+      width: '50px',
+      height: '50px',
+      borderRadius: '15px',
+      margin: '2px',
+      }
+  }
+
+ clickHandler = evt => {
+   this.setState({
+     style:{
+      backgroundColor: randomHexColor(),
+      width: '50px',
+      height: '50px',
+      borderRadius: '15px',
+      margin: '2px',
+     }
+   })
+ }
+
+ mouseEnter = evt => {
+   this.setState({
+     style:{
+        ...this.state.style,
+        backgroundColor: 'green'
+     }
+   })
+ }
+
+ doubleClick = evt => {
+  this.setState({
+    style:{
+       ...this.state.style,
+       backgroundColor: 'white'
+      }
+    })
+  }
+
+  dragEnter = evt => {
+    this.setState({
+      style:{
+         ...this.state.style,
+         backgroundColor: 'yellow'
+        }
+      })
+  }
+
+  render(){
+    return(
+      <div style={this.state.style}
+          onClick={this.clickHandler}
+          onMouseEnter={this.mouseEnter}
+          onDoubleClick={this.doubleClick}
+          onDragEnter={this.dragEnter}
+
+          >
+      </div>
+    )
+  }
+
+}
+
+export default Pixel
