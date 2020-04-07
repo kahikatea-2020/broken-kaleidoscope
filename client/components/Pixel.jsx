bf23c5bee54c694beeb67ef53fdbe17173618c12
Stan-Gogan <sjgogan@me.com>
2020-04-07 17:30:18 +1200

    kaleidoscope

---
+++ client/components/Pixel.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const randomHexColor = () =>
+  `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+
+
+class Pixel extends React.Component {
+  
+
+  constructor (props) {
+    super (props)
+
+    this.state = {
+      style: {
+        height: "100px",
+        width: "100px",
+        backgroundColor: randomHexColor(),
+      }
+    }
+
+  }
+
+  clickHandler = evt => {
+    
+    this.setState({
+      style: {
+        height: "100px",
+        width: "100px",
+        backgroundColor: randomHexColor()
+      }
+    })
+  }
+  
+  render() {
+    // const {style} =this.state
+    return (
+      <div style={this.state.style} onClick={this.clickHandler}></div>
+    )
+  }
+
+
+}
+
+
+export default Pixel
