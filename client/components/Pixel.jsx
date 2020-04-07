d06804f4c011bb8e1a775408c40815f45ecb4655
Ellora Virtue <ellora.virtue@hotmail.com>
2020-04-07 13:33:24 +1200

    Add random hex colour function and apply to style

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -3,11 +3,15 @@ import React from 'react'
 class Pixel extends React.Component {
   constructor () {
     super()
+
+    const randomHexColor = () =>
+      `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+
     this.state = {
       style: {
         height: '5em',
         width: '5em',
-        backgroundColor: 'bisque'
+        backgroundColor: randomHexColor()
       }
     }
   }
